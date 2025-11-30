56eb2af5e32032492b4f30f5bc48e4d76d7f7ff7
Dovydas <ddorofejevas@yahoo.com>
2025-11-30 17:53:22 +0200

    Initial commit

---
+++ PulseLoop/PulseLoopApp.swift
@@ -0,0 +1,32 @@
+//
+//  PulseLoopApp.swift
+//  PulseLoop
+//
+//  Created by Dovydas Dorofejevas on 30/11/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PulseLoopApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
